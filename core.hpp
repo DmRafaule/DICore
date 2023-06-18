2dbb8b242df1850bfd29a669a2d9e61403d8d83e
DmRafaule <ChedrDen@gmail.com>
2023-06-18 12:32:10 +0300

    Init DI repo/module DICore

---
+++ core.hpp
@@ -0,0 +1,37 @@
+#pragma once
+/*Precompiled headers*/
+// GL stuff
+// OpenGL stuff
+#include "GL/glew.h"
+
+// STL stuff
+#include <vector>
+#include <cmath>
+#include <string>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <memory>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include <algorithm>
+#include <functional>
+
+// GLM stuff
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// My stuff
+    #include "DIDebuger/log.hpp"
+    #include "DIDebuger/debuger.hpp"
+    #include "DITime/time.hpp"
+    #include "DIMemory/mem.hpp"
+    #include "DIUtils/utils.hpp"
